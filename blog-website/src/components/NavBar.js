cc991901d1cc17671f18d7ce6dfb76f78c2b9e15
kortmiranda <kortneymiranda@outlook.com>
2021-08-11 10:48:55 -0500

    add header banner

--- blog-website/src/components/NavBar.js
+++ blog-website/src/components/NavBar.js
@@ -10,6 +10,9 @@ function NavBar(props) {
         <div>
             <nav className="navbar">
                 <div className="nav-center">
+                    {/* <Link to="/" className="nav-logo">
+                        <img src="https://res.cloudinary.com/dl3d3eyto/image/upload/v1628634718/Kortney%20the%20Koding%20Girl/images/logos/K0rtn3y_Th3_K0d1ng_G1rl_dnv1ta.png" alt="logo" />
+                    </Link> */}
                     <ul className="nav-links">
                         <li>
                             <Link to="/">Home</Link>
--- blog-website/src/pages/Home.js
+++ blog-website/src/pages/Home.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import BlogFeed from '../components/BlogFeed'
+import '../styles/Home.css'
 
 function Home(props) {
     return (
         <div>
-            Home Page
+                <video className="header-banner" autoPlay muted>
+                    <source src="https://res.cloudinary.com/dl3d3eyto/video/upload/v1628696787/Kortney%20the%20Koding%20Girl/images/banners/blog-website-banner-1000x200_mdpiou.mp4" type="video/mp4"></source>
+                </video>
             <BlogFeed />
         </div>
     );
