c12b1c54b019138031e5f5ef6268168136c2f8f9
kortmiranda <kortneymiranda@outlook.com>
2021-08-11 13:54:29 -0500

    update home banner and favicon

--- blog-website/src/Components/Pages/Home/Home.js
+++ blog-website/src/Components/Pages/Home/Home.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import PostFeed from '../../PostFeed/PostFeed'
-import '../../PostFeed/PostFeed.css'
+import './Home.css'
 
 function Home(props) {
     return (
         <div>
-                <video className="header-banner" autoPlay muted>
-                    <source src="https://res.cloudinary.com/dl3d3eyto/video/upload/v1628696787/Kortney%20the%20Koding%20Girl/images/banners/blog-website-banner-1000x200_mdpiou.mp4" type="video/mp4"></source>
-                </video>
+            <div className="home-banner">
+                {/* <img src="https://res.cloudinary.com/dl3d3eyto/image/upload/v1628706530/Kortney%20the%20Koding%20Girl/images/banners/blog-website-lavender-banner_lb47hz.png" alt="banner" /> */}
+                <h1>Kortney the Koding Girl</h1>
+                <h3>The Blog</h3>
+                <h2>Dev tutorial, Lifestyle, War Stories</h2>
+            </div>
             <PostFeed />
         </div>
     );
