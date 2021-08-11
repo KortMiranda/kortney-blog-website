a92d9f0f7d9ff62260d1a6deeac1a6c6b56acf5f
kortmiranda <kortneymiranda@outlook.com>
2021-08-11 16:42:57 -0500

    add post preview grid

--- blog-website/src/Components/PostFeed/PostFeed.js
+++ blog-website/src/Components/PostFeed/PostFeed.js
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react';
-// import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import './PostFeed.css'
 import axios from 'axios'
+import Grid from "@material-ui/core/Grid";
+import { faCaretDown } from '@fortawesome/free-solid-svg-icons';
 
 export default function BlogFeed(props) {
     const [feed, setFeed] = useState([])
@@ -16,9 +19,54 @@ export default function BlogFeed(props) {
         }
         getFeed()
     }, [])
+
+
     return (
-        <div>
-            
+        <div className="feed-grid">
+            {feed.map((preview) => {
+                if(!preview.image) {
+                    return(
+                        <div className="post-item">
+                            <div className="no-image">
+                                <Link to={`post/${preview.id}`} key={faCaretDown.id}>
+                                <h3>{preview.title}</h3>
+                                <p>{preview.natural_time}</p>
+                                </Link>
+                            </div>
+                        </div>
+                    )
+                } else {
+                return(
+                    <div className="post-item">
+                        <Link to={`post/${preview.id}`} key={faCaretDown.id}>
+                            <img src={preview.image} alt={preview.title}/>
+                            {/* <h3>{preview.title}</h3> */}
+                            {/* <p>{preview.natural_time}</p> */}
+                        </Link>
+                    </div>
+                )
+                }
+                // if(!preview.image) {
+                //     return(
+                //         <Grid item xs={0} sm={0} md={0} lg={0} className="post-item">
+                //         <Link to={`post/${preview.id}`} key={faCaretDown.id}>
+                //             <h3>{preview.title}</h3>
+                //             <p>{preview.natural_time}</p>
+                //         </Link>
+                //         </Grid>
+                //     )
+                // } else {
+                // return(
+                //     <Grid item md={5} className="post-item">
+                //         <Link to={`post/${preview.id}`} key={faCaretDown.id}>
+                //             <img src={preview.image} alt={preview.title}/>
+                //             <h3>{preview.title}</h3>
+                //             <p>{preview.natural_time}</p>
+                //         </Link>
+                //     </Grid>
+                // )
+                // }
+            })}
         </div>
     );
 }
